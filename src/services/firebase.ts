6b2877e8499b148dbffdf57db8f058c32729196e
Blossom Parris <blossomparris@blossoms-mbp.mynetworksettings.com>
2026-07-08 21:19:02 -0400

    Add Firebase project setup

---
+++ src/services/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwqs0yZTJib_3XKFWTU-AyEuuPkRdqjqc",
+  authDomain: "tripmuse-bd0ba.firebaseapp.com",
+  projectId: "tripmuse-bd0ba",
+  storageBucket: "tripmuse-bd0ba.firebasestorage.app",
+  messagingSenderId: "1093723039753",
+  appId: "1:1093723039753:web:096ec293150c7daec470d8",
+  measurementId: "G-ZEH14RMHYP",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
